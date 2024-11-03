f26def759e49f6d73d0b9605aa5dd7a7c909a8e3
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2024-11-03 10:43:43 +0530

    Hero component initial phase

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <section>
+            <h1>
+                Hi there <br /> I am Prabhu Palani
+                <p> I am an Full stack developer</p>
+            </h1>
+        </section>
+    );
+}
