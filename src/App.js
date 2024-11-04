d20f257fd13476d33ca13bdda36ff648a74e26cb
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2024-11-04 22:58:59 +0530

    merged conflicts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
 import Header from './components/Header';
-<<<<<<< Updated upstream
-=======
 import Hero from './components/Hero';
 import About from './components/About';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <Header/>
-<<<<<<< Updated upstream
-=======
       <Hero/>
       <About/>
->>>>>>> Stashed changes
     </div>
   );
 }
